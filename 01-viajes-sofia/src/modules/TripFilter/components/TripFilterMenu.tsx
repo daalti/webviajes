23586bde77df193389351f647c250c08dc1d3b25
Daniel Alonso Tirapu <daaltigo@gmail.com>
2025-01-01 16:56:08 +0100

    improving filter

--- 01-viajes-sofia/src/modules/TripFilter/components/TripFilterMenu.tsx
+++ 01-viajes-sofia/src/modules/TripFilter/components/TripFilterMenu.tsx
@@ -1,14 +1,45 @@
 import "./TripFilterMenu.css";
 
+const trips = [
+  {
+    id: 1,
+    title: "Trip 1",
+    image: "https://via.placeholder.com/150",
+    number: 5,
+  },
+  {
+    id: 2,
+    title: "Trip 2",
+    image: "https://via.placeholder.com/150",
+    number: 10,
+  },
+  {
+    id: 3,
+    title: "Trip 3",
+    image: "https://via.placeholder.com/150",
+    number: 8,
+  },
+  {
+    id: 4,
+    title: "Trip 4",
+    image: "https://via.placeholder.com/150",
+    number: 7,
+  },
+];
+
 export const TripFilterMenu: React.FC = () => {
   return (
     <div className="trip-filter-menu-wrapper">
       <div className="trip-filter-menu-container">
         <ul className="trip-filter-menu">
-          <li className="trip-filter-menu-item">Filter 1</li>
-          <li className="trip-filter-menu-item">Filter 2</li>
-          <li className="trip-filter-menu-item">Filter 3</li>
-          <li className="trip-filter-menu-item">Filter 4</li>
+          {trips.map((trip) => (
+            <li key={trip.id} className="trip-filter-menu-item">
+              <button className="trip-filter-menu-button">
+                <span>{trip.title}</span>
+                {trip.number}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
