1a982cd31ab080b765f371729337032f57e61e86
Daniel Alonso Tirapu <daaltigo@gmail.com>
2025-01-03 20:32:22 +0100

    fixing menu in egipt page

--- 01-viajes-sofia/src/Pages/TripMenu/EgiptPage.tsx
+++ 01-viajes-sofia/src/Pages/TripMenu/EgiptPage.tsx
@@ -1,21 +1,30 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import "./EgiptPage.css";
-
 import { FC } from "react";
+import { MenuButton } from "../../modules/Menu/components/MenuButton";
+import { Menu } from "../../modules/Menu/components/Menu";
 
 export const EgiptPage: FC = () => {
   const location = useLocation();
   const { image, prevPath } = location.state;
   const navigate = useNavigate();
 
+  const handleMenuButtonClick = (): void => {
+    navigate(prevPath);
+  };
+
   return (
-    <div className="full-image-view">
-      <img
-        src={image.src}
-        alt={image.alt}
-        className="full-image"
-        onClick={() => navigate(prevPath)}
-      />
-    </div>
+    <>
+      <div className="full-image-view">
+        <img
+          src={image.src}
+          alt={image.alt}
+          className="full-image"
+          onClick={() => navigate(prevPath)}
+        />
+      </div>
+      <MenuButton onChangeMenu={handleMenuButtonClick} menuOpen={false} />
+      <Menu menuOpen={false} />
+    </>
   );
 };
