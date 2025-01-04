2bc4fe40ce0e9a0adc55c199d59f013174bbf681
Daniel Alonso Tirapu <daaltigo@gmail.com>
2025-01-04 12:42:40 +0100

    Adding egipt draft images

--- 01-viajes-sofia/src/Pages/TripMenu/EgiptPage.tsx
+++ 01-viajes-sofia/src/Pages/TripMenu/EgiptPage.tsx
@@ -1,8 +1,9 @@
-import { useLocation, useNavigate } from "react-router-dom";
 import "./EgiptPage.css";
 import { FC } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { MenuButton } from "../../modules/Menu/components/MenuButton";
 import { Menu } from "../../modules/Menu/components/Menu";
+import imagesData from "../../assets/data/egipt_images.json";
 
 export const EgiptPage: FC = () => {
   const location = useLocation();
@@ -15,14 +16,22 @@ export const EgiptPage: FC = () => {
 
   return (
     <>
-      <div className="full-image-view">
-        <img
-          src={image.src}
-          alt={image.alt}
-          className="full-image"
-          onClick={() => navigate(prevPath)}
-        />
+      <div className="scroll-container">
+        <div className="full-image-view">
+          <img
+            src={image.src}
+            alt={image.alt}
+            className="full-image"
+            onClick={() => navigate(prevPath)}
+          />
+        </div>
       </div>
+
+      <section className="images-container">
+        {imagesData.images.map((img, index) => (
+          <img key={`col1-${index}`} src={img.src} alt={img.alt} />
+        ))}
+      </section>
       <MenuButton onChangeMenu={handleMenuButtonClick} menuOpen={false} />
       <Menu menuOpen={false} />
     </>
